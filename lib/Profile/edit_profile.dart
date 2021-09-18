e222d2c0e49182c84d92e52d8d7f588d9d98f5f9
Ovidiu Toma <ovidiutoma876@gmail.com>
2021-09-18 16:38:39 +0300

    Remade the whole "Edit Profile" page

--- lib/Profile/edit_profile.dart
+++ lib/Profile/edit_profile.dart
@@ -1,11 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/painting.dart';
-import 'package:personal_app/Profile/profile_widget.dart';
 import 'package:personal_app/User_things/user.dart';
 import 'package:personal_app/User_things/user_preferences.dart';
-import 'package:personal_app/Widget/appbar_widget.dart';
-import 'package:personal_app/Widget/textfield_widget.dart';
 
 class EditProfilePage extends StatefulWidget {
   const EditProfilePage({Key? key}) : super(key: key);
@@ -16,80 +12,101 @@ class EditProfilePage extends StatefulWidget {
 
 class _EditProfilePageState extends State<EditProfilePage> {
   User user = UserPreferences.myUser;
-  TextEditingController _textEditingController = TextEditingController();
 
   @override
-  Widget build(BuildContext context) => Scaffold(
-        appBar: buildAppBar(context),
-        body: ListView(
-          padding: EdgeInsets.symmetric(horizontal: 32),
-          physics: BouncingScrollPhysics(),
-          children: [
-            ProfileWidget(
-              imagePath: user.imagePath,
-              isEdit: true,
-              onClicked: () async {},
-            ),
-            const SizedBox(height: 24),
-            TextField(
-              decoration: InputDecoration(
-                labelText: "Full Name",
-                labelStyle: TextStyle(
-                  fontWeight: FontWeight.bold,
-                  fontSize: 20,
-                  color: Colors.black,
-                ),
-                floatingLabelBehavior: FloatingLabelBehavior.always,
-                hintText: user.name,
-                hintStyle: TextStyle(
-                  fontSize: 16,
-                ),
-              ),
-              onChanged: (name) {},
-            ),
-            const SizedBox(height: 24),
-            TextField(
-              decoration: InputDecoration(
-                labelText: "Email",
-                labelStyle: TextStyle(
-                  fontWeight: FontWeight.bold,
-                  fontSize: 20,
-                  color: Colors.black,
-                ),
-                floatingLabelBehavior: FloatingLabelBehavior.always,
-                hintText: user.email,
-                hintStyle: TextStyle(
-                  fontSize: 16,
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).scaffoldBackgroundColor,
+        elevation: 1,
+        leading: IconButton(
+          icon: Icon(
+            Icons.arrow_back,
+            color: Colors.blue,
+          ),
+          onPressed: () {},
+        ),
+        actions: [
+          IconButton(
+              onPressed: () {},
+              icon: Icon(
+                Icons.settings,
+                color: Colors.blue,
+              ))
+        ],
+      ),
+      body: Container(
+        padding: EdgeInsets.only(left: 16, top: 25, right: 16),
+        child: GestureDetector(
+          onTap: () {
+            FocusScope.of(context).unfocus();
+          },
+          child: ListView(
+            children: [
+              Text(
+                "Edit Profile",
+                style: TextStyle(
+                  fontSize: 25,
+                  fontWeight: FontWeight.w500,
                 ),
               ),
-              onChanged: (email) {},
-            ),
-            const SizedBox(height: 24),
-            TextField(
-              decoration: InputDecoration(
-                labelText: "About",
-                labelStyle: TextStyle(
-                  fontWeight: FontWeight.bold,
-                  fontSize: 20,
-                  color: Colors.black,
-                ),
-                floatingLabelBehavior: FloatingLabelBehavior.always,
-                hintText: user.about,
-                hintStyle: TextStyle(
-                  fontSize: 16,
+              SizedBox(
+                height: 15,
+              ),
+              Center(
+                child: Stack(
+                  children: [
+                    Container(
+                        width: 130,
+                        height: 130,
+                        decoration: BoxDecoration(
+                          border: Border.all(
+                              color: Theme.of(context).scaffoldBackgroundColor),
+                          boxShadow: [
+                            BoxShadow(
+                                spreadRadius: 2,
+                                blurRadius: 10,
+                                color: Colors.black.withOpacity(0.1),
+                                offset: Offset(0, 10))
+                          ],
+                          shape: BoxShape.circle,
+                          image: DecorationImage(
+                              fit: BoxFit.cover,
+                              image: NetworkImage(user.imagePath)),
+                        )),
+                    Positioned(
+                        bottom: 0,
+                        right: 0,
+                        child: Container(
+                          height: 40,
+                          width: 40,
+                          decoration: BoxDecoration(
+                              shape: BoxShape.circle,
+                              border: Border.all(
+                                  width: 4,
+                                  color: Theme.of(context)
+                                      .scaffoldBackgroundColor),
+                              color: Colors.blue),
+                          child: Icon(
+                            Icons.edit,
+                            color: Colors.white,
+                          ),
+                        ))
+                  ],
                 ),
               ),
-              maxLines: 5,
-              onChanged: (about) {},
-            ),
-            SizedBox(
-              height: 35,
-            ),
-            Row(
+              SizedBox(height: 35,),
+              buildTextField("Full Name", user.name),
+              buildTextField("E-mail", user.email),
+              buildTextField("About", user.about),
+              SizedBox(height: 35,),
+              Row(
               mainAxisAlignment: MainAxisAlignment.spaceAround,
               children: [
                 OutlinedButton(
-                    onPressed: () {},
+                    onPressed: () {
+                      
+                    },
                     child: Text(
                       "CANCEL",
                       style: TextStyle(
@@ -108,10 +125,34 @@ class _EditProfilePageState extends State<EditProfilePage> {
                           fontSize: 14,
                           letterSpacing: 2.2,
                           color: Colors.white),
-                    ))
-              ],
-            ),
-          ],
+                    )
+                    )
+              ]),
+              
+            ],
+          ),
         ),
-      );
+      ),
+    );
+  }
+
+  Widget buildTextField(String labelText, String placeholder) {
+    return Padding(
+      padding: const EdgeInsets.only(bottom: 35),
+      child: TextField(
+                decoration: InputDecoration(
+                  contentPadding: EdgeInsets.only(bottom: 5),
+                  labelText: labelText,
+                  labelStyle: TextStyle(fontSize: 20),
+                  floatingLabelBehavior: FloatingLabelBehavior.always,
+                  hintText: placeholder,
+                  hintStyle: TextStyle(
+                    fontSize: 16,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.black,
+                  ),
+                ),
+              ),
+    );
+  }
 }
